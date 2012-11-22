b7d9e414719ccb701228be62f09fc87e54356e54
Bruno-Muller <muller.bru@gmail.com>
2012-11-22 22:23:54 +0100

    no message

--- src/mining/ui/Controller.java
+++ src/mining/ui/Controller.java
@@ -88,6 +88,7 @@ public class Controller implements ActionListener, ItemListener, ListSelectionLi
 			frame.setPreferredSize(new Dimension(300, 600));
 			frame.pack();
 			frame.setVisible(true);	
+			// test
 		}
 		
 	}
